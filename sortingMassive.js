a74c22e6896d680da9f6a45ee7aa0cd4ed617000
Maxim <39403500+Maximile0n@users.noreply.github.com>
2018-05-20 15:02:51 +0300

    rename lengthMassive

--- sortingMassive.js
+++ sortingMassive.js
@@ -5,11 +5,11 @@ var test2 = [121, 150, 163, 207, 203, 189, 222];
 
 //сортировка массива по возрастанию
 var sortingMassiveUp = function(massive){
-	var dlin = massive.length;
+	var lengthMassive = massive.length;
 	var minValue;
-	for (var i = 0; i <= dlin - 2; i++) {
+	for (var i = 0; i <= lengthMassive - 2; i++) {
 		var minValue = massive[i];
-		for (var j = i + 1; j <= dlin - 1; j++) {
+		for (var j = i + 1; j <= lengthMassive - 1; j++) {
 			if (massive[j] < minValue) {
 			minValue = massive[j];
 			var swap = massive[i];
@@ -20,11 +20,11 @@ var sortingMassiveUp = function(massive){
 }
 // сортировка массива по убыванию
 var sortingMassiveDown = function(massive){
-	var dlin = massive.length;
+	var lengthMassive = massive.length;
 	var minValue;
-	for (var i = 0; i <= dlin - 2; i++) {
+	for (var i = 0; i <= lengthMassive - 2; i++) {
 		var minValue = massive[i];
-		for (var j = i + 1; j <= dlin - 1; j++) {
+		for (var j = i + 1; j <= lengthMassive - 1; j++) {
 			if (massive[j] > minValue) {
 			minValue = massive[j];
 			var swap = massive[i];
